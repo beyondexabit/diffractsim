2341bc602ae18ba5dcda217639928ba15afbbb75
Jakub Kostial <jakubkostial@eduroam-int-dhcp-97-166-137.ucl.ac.uk>
2024-03-15 10:36:16 +0000

    make clean

--- beamsteer.py
+++ beamsteer.py
@@ -21,69 +21,27 @@ F = MonochromaticField(
 )
 
 #SMF amplitude mask
-#F.add(ApertureFromArray(amplitude_mask = "/Users/jakubkostial/Documents/phd/code/onn_simulation/smf_amplitude_mask.npy", image_size=(13854 * um, 8640 * um), simulation = F))
-F.add(ApertureFromImage(amplitude_mask_path= "/Users/jakubkostial/Documents/phd/code/dsim/diffractsim/examples/apertures/white_background.png", simulation = F))
+F.add(ApertureFromArray(amplitude_mask = "/Users/jakubkostial/Documents/phd/code/onn_simulation/smf_amplitude_mask.npy", image_size=(13854 * um, 8640 * um), simulation = F))
+#F.add(ApertureFromImage(amplitude_mask_path= "/Users/jakubkostial/Documents/phd/code/dsim/diffractsim/examples/apertures/white_background.png", simulation = F))
 
 distance = 10*cm
 #F.propagate(distance)
 
 # SLM
-
-F.add(ApertureFromArray(phase_mask = "/Users/jakubkostial/Documents/phd/code/slm_matrix_gen-main/matrices_dsim_scaling/l1/fix_gratingpattern.npy", image_size=(15360 * um, 8640 * um), simulation = F))
-
-#F.add(ApertureFromArray(phase_mask = "/Users/jakubkostial/Documents/phd/code/slm_matrix_gen-main/matrices_dsim_scaling/l1/phase_pattern_l1_20gfx-46gfy_ones.npy", image_size=(15360 * um, 8640 * um), simulation = F))
+F.add(ApertureFromArray(phase_mask = "/Users/jakubkostial/Documents/phd/code/slm_matrix_gen-main/formatted_matrices_individual/l1/phase_pattern_l1_20gfx-46gfy_00_10_00_test.npy", image_size=(15360 * um, 8640 * um), simulation = F))
 
 F.propagate(10*cm)
 # take Fourier transform
 F.add(Lens(f = 10*cm))
 F.propagate(10*cm)
 
-F.propagate(1*cm)
 # aperture
-F.add(ApertureFromArray(amplitude_mask = "/Users/jakubkostial/Documents/phd/code/ff_project/matrix_generation_dmd/repo/newgrating.npy", image_size=(30 * mm, 30 * mm), simulation = F))
-#F.propagate(0.2*cm)
+F.add(ApertureFromArray(amplitude_mask = "/Users/jakubkostial/Documents/phd/code/dsim/myholos/aperture1_l.npy", image_size=(30 * mm, 30 * mm), simulation = F))
+
 I1 = F.get_intensity()
 plot_field(I1, "First Holo 1 output")
 
-
-
-F.add(ApertureFromArray(phase_mask = "/Users/jakubkostial/Documents/phd/code/slm_matrix_gen-main/matrices_dsim_scaling/l1/global_mask.npy", image_size=(15360 * um, 8640 * um), simulation = F))
-#F.add(ApertureFromArray(phase_mask = "/Users/jakubkostial/Documents/phd/code/slm_matrix_gen-main/matrices_dsim_scaling/l1/phase_pattern_l1_20gfx-46gfy_ones.npy", image_size=(15360 * um, 8640 * um), simulation = F))
-'''
-I1 = F.get_intensity()
-#plot_field(I1, "output at 2nd phase mask")
-F.propagate(10*cm)
-I1 = F.get_intensity()
-plot_field(I1, "output at 2nd phase mask after propagation")
-'''
-
-
-F.add(Lens(f = 10*cm))
-F.propagate(10*cm)
-I1 = F.get_intensity()
-plot_field(I1, "First Holo 1 output")
-
-
-F.propagate(10*cm)
-I1 = F.get_intensity()
-#plot_field(I1, "First Holo 1 output")
-
-F.propagate(30*cm)
-I1 = F.get_intensity()
-plot_field(I1, "First Holo 1 output")
-
+F.plot_intensity(I1, square_root = True, units = mm, grid = True, figsize = (14,5), use_log_scale = True)
 
 plt.show()
 sys.exit()
-
-
-F.add(ApertureFromArray(phase_mask = "/Users/jakubkostial/Documents/phd/code/slm_matrix_gen-main/matrices_dsim_scaling/l1/fix_gratingpattern.npy", image_size=(30. * mm, 30. * mm), simulation = F))
-F.add(Lens(f = 10*cm))
-F.propagate(10*cm)
-
-I1 = F.get_intensity()
-plot_field(I1, "First Holo 1 output")
-
-            
-
-
